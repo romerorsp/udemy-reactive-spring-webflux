52bbffd1c1b1c07e1faf914e3e148c24eeb110dc
Rômero Ricardo <romerorr16@msn.com>
2022-05-28 16:59:48 -0400

    All the examples given by the 32 lectures of Exploring Flux and Mono (hands on)

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -1,4 +1,191 @@
 package com.learnreactiveprogramming.service;
 
+import java.time.Duration;
+import java.util.List;
+import java.util.Random;
+import java.util.function.Function;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
 public class FluxAndMonoGeneratorService {
+
+  public static void main(String[] args) {
+    var service = new FluxAndMonoGeneratorService();
+
+    service.namesFlux()
+        .subscribe(name -> System.out.printf("Name is: %s\n", name));
+    service.nameMono()
+        .subscribe(name -> System.out.printf("Family is: %s\n", name));
+  }
+
+  public Flux<String> namesFlux() {
+    return Flux.fromIterable(List.of("Rômero", "Bianca", "Shauana"));
+  }
+
+  public Flux<String> namesFluxMap() {
+    return namesFlux()
+        .map(String::toUpperCase)
+        .log();
+  }
+
+  public Flux<String> namesFluxTransform() {
+    Function<Flux<String>, Flux<String>> transformer = flux -> flux.map(String::toUpperCase);
+    return namesFlux()
+        .transform(transformer)
+        .log();
+  }
+
+  public Flux<String> namesFluxFiltered() {
+    return namesFlux()
+        .filter(name -> name.length() == 6)
+        .map(String::toUpperCase)
+        .log();
+  }
+
+  public Flux<String> namesFluxFilteredDefaultIfEmpty(int nameLength) {
+    return namesFlux()
+        .filter(name -> name.length() == nameLength)
+        .map(String::toUpperCase)
+        .defaultIfEmpty("default")
+        .log();
+  }
+
+  public Flux<String> namesFluxFilteredSwitchIfEmpty(int nameLength) {
+    Function<Flux<String>, Flux<String>> mapper = flux -> flux.map(String::toUpperCase);
+    Flux<String> transform = Flux.just("default").transform(mapper);
+    return namesFlux()
+        .filter(name -> name.length() == nameLength)
+        .transform(mapper)
+        .switchIfEmpty(transform)
+        .log();
+  }
+
+  public Flux<String> namesFluxMappedFiltered() {
+    return namesFlux()
+        .filter(name -> name.length() == 6)
+        .map(String::toUpperCase)
+        .map(name -> "%d-%s".formatted(name.length(), name))
+        .log();
+  }
+
+  public Flux<String> namesFluxImmutability() {
+    var namesFlux = namesFlux().log();
+    //noinspection ReactiveStreamsUnusedPublisher
+    namesFlux.map(String::toUpperCase);
+    return namesFlux;
+  }
+
+  public Mono<String> nameMono() {
+    return namesFlux()
+        .reduce("%s, %s"::formatted)
+        .log();
+  }
+
+  public Mono<List<Character>> nameMonoFlatMapped() {
+    return nameMono()
+        .flatMap(name -> Mono.just(name.chars().mapToObj(c -> (char) c).toList()))
+        .log();
+  }
+
+  public Flux<Character> nameMonoFlatMappedMany() {
+    return nameMono()
+        .flatMapMany(name -> Flux.fromStream(name.chars().mapToObj(c -> (char) c)))
+        .log();
+  }
+
+  public Flux<Character> namesFluxFlatMappedFiltered() {
+    return namesFluxFiltered()
+        .flatMap(this::nameToCharacterFlux)
+        .log();
+  }
+
+  public Flux<Character> namesFluxFlatMappedFilteredAsync() {
+    return namesFluxFiltered()
+        .flatMap(name -> nameToCharacterFlux(name)
+            .delayElements(
+                Duration.ofMillis(
+                    new Random(1000).nextInt()
+                )
+            )
+        ).log();
+  }
+
+  public Flux<Character> namesFluxConcatMappedFiltered() {
+    return namesFluxFiltered()
+        .concatMap(name -> nameToCharacterFlux(name)
+            .delayElements(
+                Duration.ofMillis(
+                    new Random(1000).nextInt()
+                )
+            )
+        ).log();
+  }
+
+  public Flux<Character> alphabetFluxConcat() {
+    return Flux.concat(Flux.just('A', 'B', 'C'), Flux.just('D', 'E', 'F'));
+  }
+
+  public Flux<Character> alphabetFluxConcatWith() {
+    return Flux.just('A', 'B', 'C').concatWith(Flux.just('D', 'E', 'F'));
+  }
+
+  public Flux<Character> alphabetMonoConcatWith() {
+    return Mono.just('A').concatWith(Mono.just('B'));
+  }
+
+  private Flux<Character> nameToCharacterFlux(String name) {
+    return Flux.fromStream(name.chars().mapToObj(c -> (char) c));
+  }
+
+  public Flux<Character> alphabetFluxMerge() {
+    return Flux.merge(
+        Flux.just('A', 'B', 'C')
+            .delayElements(Duration.ofMillis(100)),
+        Flux.just('D', 'E', 'F')
+            .delayElements(Duration.ofMillis(125))
+    ).log();
+  }
+
+  public Flux<Character> alphabetFluxMergeSequential() {
+    return Flux.mergeSequential(
+        Flux.just('A', 'B', 'C')
+            .delayElements(Duration.ofMillis(100)),
+        Flux.just('D', 'E', 'F')
+            .delayElements(Duration.ofMillis(125))
+    ).log();
+  }
+
+  public Flux<Character> alphabetFluxMergeWith() {
+    return Flux.just('A', 'B', 'C')
+        .delayElements(Duration.ofMillis(100))
+        .mergeWith(Flux.just('D', 'E', 'F').delayElements(Duration.ofMillis(125)))
+        .log();
+  }
+
+  public Flux<Character> alphabetMonoMergeWith() {
+    return Mono.just('A').mergeWith(Mono.just('B')).log();
+  }
+
+  public Flux<String> alphabetFluxZip() {
+    return Flux.zip(
+        Flux.just('A', 'B', 'C')
+            .delayElements(Duration.ofMillis(100)),
+        Flux.just('D', 'E', 'F')
+            .delayElements(Duration.ofMillis(125)),
+        (char1, char2) -> "%c%c".formatted(char1, char2)
+    ).log();
+  }
+
+  public Flux<String> alphabetFluxZipWith() {
+    return Flux.just('A', 'B', 'C')
+        .delayElements(Duration.ofMillis(100))
+        .zipWith(
+            Flux.just('D', 'E', 'F').delayElements(Duration.ofMillis(125)),
+            "%c%c"::formatted)
+        .log();
+  }
+
+  public Mono<String> alphabetMonoZipWith() {
+    return Mono.just('A').zipWith(Mono.just('B'), "%c%c"::formatted).log();
+  }
 }
--- reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
+++ reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
@@ -1,7 +1,281 @@
 package com.learnreactiveprogramming.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import reactor.test.StepVerifier;
 
 class FluxAndMonoGeneratorServiceTest {
 
+  private FluxAndMonoGeneratorService fixture;
+
+  @BeforeEach
+  public void setup() {
+    fixture = new FluxAndMonoGeneratorService();
+  }
+
+  @Test
+  public void testNamesFlux() {
+    var namesFlux = fixture.namesFlux();
+
+    StepVerifier.create(namesFlux)
+        .expectNext("Rômero", "Bianca", "Shauana")
+        .verifyComplete();
+
+    StepVerifier.create(namesFlux)
+        .expectNextCount(3)
+        .verifyComplete();
+
+    StepVerifier.create(namesFlux)
+        .expectNext("Rômero")
+        .expectNextCount(2)
+        .verifyComplete();
+  }
+
+  @Test
+  public void testNameMono() {
+    var nameMono = fixture.nameMono();
+
+    StepVerifier.create(nameMono)
+        .expectNext("Rômero, Bianca, Shauana")
+        .verifyComplete();
+  }
+
+  @Test
+  public void testNameMonoFlatMapped() {
+    var familyMono = fixture.nameMonoFlatMapped();
+
+    StepVerifier.create(familyMono)
+        .expectNext(
+            List.of(
+                'R', 'ô', 'm', 'e', 'r', 'o', ',', ' ', 'B', 'i', 'a', 'n', 'c', 'a', ',',
+                ' ', 'S', 'h', 'a', 'u', 'a', 'n', 'a')
+        ).verifyComplete();
+  }
+
+  @Test
+  public void testNameMonoFlatMappedMany() {
+    var familyFlux = fixture.nameMonoFlatMappedMany();
+
+    StepVerifier.create(familyFlux)
+        .expectNext(
+            'R', 'ô', 'm', 'e', 'r', 'o', ',', ' ', 'B', 'i', 'a', 'n', 'c', 'a', ',',
+            ' ', 'S', 'h', 'a', 'u', 'a', 'n', 'a')
+        .verifyComplete();
+  }
+
+  @Test
+  public void testNamesFluxMap() {
+    var namesFlux = fixture.namesFluxMap();
+
+    StepVerifier.create(namesFlux)
+        .expectNext("RÔMERO", "BIANCA", "SHAUANA")
+        .verifyComplete();
+  }
+
+  @Test
+  public void testNamesFluxTransform() {
+    var namesFlux = fixture.namesFluxTransform();
+
+    StepVerifier.create(namesFlux)
+        .expectNext("RÔMERO", "BIANCA", "SHAUANA")
+        .verifyComplete();
+  }
+
+  @Test
+  public void testNamesFluxImmutability() {
+    var namesFlux = fixture.namesFluxImmutability();
+
+    StepVerifier.create(namesFlux)
+        .expectNext("Rômero", "Bianca", "Shauana")
+        .verifyComplete();
+  }
+
+  @Test
+  public void testNamesFluxFiltered() {
+    var namesFlux = fixture.namesFluxFiltered();
+
+    StepVerifier.create(namesFlux)
+        .expectNext("RÔMERO", "BIANCA")
+        .verifyComplete();
+  }
+
+  @Test
+  public void testNamesFluxFilteredDefaultIfEmpty() {
+    var namesFlux = fixture.namesFluxFilteredDefaultIfEmpty(10);
+
+    StepVerifier.create(namesFlux)
+        .expectNext("default")
+        .verifyComplete();
+  }
+
+  @Test
+  public void testNamesFluxFilteredSwitchIfEmpty() {
+    var namesFlux = fixture.namesFluxFilteredSwitchIfEmpty(10);
+
+    StepVerifier.create(namesFlux)
+        .expectNext("DEFAULT")
+        .verifyComplete();
+  }
+
+  @Test
+  public void testNamesFluxMappedFiltered() {
+    var namesFlux = fixture.namesFluxMappedFiltered();
+
+    StepVerifier.create(namesFlux)
+        .expectNext("6-RÔMERO", "6-BIANCA")
+        .verifyComplete();
+  }
+
+  @Test
+  public void testNamesFluxFlatMappedFiltered() {
+    var namesFlux = fixture.namesFluxFlatMappedFiltered();
+
+    StepVerifier.create(namesFlux)
+        .expectNext('R', 'Ô', 'M', 'E', 'R', 'O', 'B', 'I', 'A', 'N', 'C', 'A')
+        .verifyComplete();
+  }
+
+  @Test
+  public void testNamesFluxFlatMappedFilteredAsync() {
+    var namesFlux = fixture.namesFluxFlatMappedFilteredAsync();
+
+    StepVerifier.create(namesFlux)
+        .expectNextCount(12)
+        .verifyComplete();
+
+    final List<Character> expected = new ArrayList<>(
+        List.of('R', 'Ô', 'M', 'E', 'R', 'O', 'B', 'I', 'A', 'N', 'C', 'A')
+    );
+
+    final Predicate<Character> matcher = next -> {
+      try {
+        return expected.contains(next);
+      } finally {
+        expected.remove(next);
+      }
+    };
+
+    StepVerifier.create(namesFlux)
+        .expectNextMatches(matcher)
+        .expectNextMatches(matcher)
+        .expectNextMatches(matcher)
+        .expectNextMatches(matcher)
+        .expectNextMatches(matcher)
+        .expectNextMatches(matcher)
+        .expectNextMatches(matcher)
+        .expectNextMatches(matcher)
+        .expectNextMatches(matcher)
+        .expectNextMatches(matcher)
+        .expectNextMatches(matcher)
+        .expectNextMatches(matcher)
+        .verifyComplete();
+    Assertions.assertTrue(expected.isEmpty());
+  }
+
+  @Test
+  public void testNamesFluxConcatMappedFiltered() {
+    var namesFlux = fixture.namesFluxConcatMappedFiltered();
+
+    StepVerifier.create(namesFlux)
+        .expectNextCount(12)
+        .verifyComplete();
+
+    StepVerifier.create(namesFlux)
+        .expectNext('R', 'Ô', 'M', 'E', 'R', 'O', 'B', 'I', 'A', 'N', 'C', 'A')
+        .verifyComplete();
+  }
+
+  @Test
+  public void testFluxConcat() {
+    var alphabetFlux = fixture.alphabetFluxConcat();
+
+    StepVerifier.create(alphabetFlux)
+        .expectNext('A', 'B', 'C', 'D', 'E', 'F')
+        .verifyComplete();
+  }
+
+  @Test
+  public void testFluxConcatWith() {
+    var alphabetFlux = fixture.alphabetFluxConcatWith();
+
+    StepVerifier.create(alphabetFlux)
+        .expectNext('A', 'B', 'C', 'D', 'E', 'F')
+        .verifyComplete();
+  }
+
+  @Test
+  public void testMonoConcatWith() {
+    var alphabetFlux = fixture.alphabetMonoConcatWith();
+
+    StepVerifier.create(alphabetFlux)
+        .expectNext('A', 'B')
+        .verifyComplete();
+  }
+
+  @Test
+  public void testFluxMerge() {
+    var alphabetFlux = fixture.alphabetFluxMerge();
+
+    StepVerifier.create(alphabetFlux)
+        .expectNext('A', 'D', 'B', 'E', 'C', 'F')
+        .verifyComplete();
+  }
+
+  @Test
+  public void testFluxMergeSequential() {
+    var alphabetFlux = fixture.alphabetFluxMergeSequential();
+
+    StepVerifier.create(alphabetFlux)
+        .expectNext('A', 'B', 'C', 'D', 'E', 'F')
+        .verifyComplete();
+  }
+
+  @Test
+  public void testFluxMergeWith() {
+    var alphabetFlux = fixture.alphabetFluxMergeWith();
+
+    StepVerifier.create(alphabetFlux)
+        .expectNext('A', 'D', 'B', 'E', 'C', 'F')
+        .verifyComplete();
+  }
+
+  @Test
+  public void testMonoMergeWith() {
+    var alphabetFlux = fixture.alphabetMonoMergeWith();
+
+    StepVerifier.create(alphabetFlux)
+        .expectNext('A', 'B')
+        .verifyComplete();
+  }
+
+  @Test
+  public void testFluxZip() {
+    var alphabetFlux = fixture.alphabetFluxZip();
+
+    StepVerifier.create(alphabetFlux)
+        .expectNext("AD", "BE", "CF")
+        .verifyComplete();
+  }
+
+  @Test
+  public void testFluxZipWith() {
+    var alphabetFlux = fixture.alphabetFluxZipWith();
+
+    StepVerifier.create(alphabetFlux)
+        .expectNext("AD", "BE", "CF")
+        .verifyComplete();
+  }
+
+  @Test
+  public void testMonoZipWith() {
+    var alphabetMono = fixture.alphabetMonoZipWith();
+
+    StepVerifier.create(alphabetMono)
+        .expectNext("AB")
+        .verifyComplete();
+  }
 }
